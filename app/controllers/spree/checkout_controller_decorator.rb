87a2baf8b6259ea28d7c8e4b1d65994ffa806762
qinghe <david@getstore.cn>
2017-12-13 21:28:08 -0500

    support rails 5.1

--- app/controllers/spree/checkout_controller_decorator.rb
+++ app/controllers/spree/checkout_controller_decorator.rb
@@ -1,7 +1,7 @@
 #encoding: utf-8
 module Spree
   CheckoutController.class_eval do
-    before_filter :checkout_hook, :only => [:update]
+    before_action :checkout_hook, :only => [:update]
 
     private
 
